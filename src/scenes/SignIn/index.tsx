67bc38d79b8f9a4ddc5ddd2c55a5b8ecbc2a2fcb
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-10-15 11:52:26 -0300

    feat(scene): finish layout and api integration on signin

--- src/scenes/SignIn/index.tsx
+++ src/scenes/SignIn/index.tsx
@@ -1,12 +1,124 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import * as Yup from 'yup';
 
-import { Container } from './styles';
+import Button from '@components/Button';
+import Input from '@components/Input';
+import Spacing from '@components/Spacing';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { signInRequest, errorResponse } from '@store/slices/User';
+import { isIOS, showAlert } from '@utils/helpers';
 
-const SignIn = () => (
-  <Container testID="viewsignin">
-    <Text>SignIn</Text>
-  </Container>
-);
+import { Container, Logo, Content, Title, SubTitle, KeyboardContent, ErrorText } from './styles';
+
+const background = require('@assets/bg.png');
+const logo = require('@assets/logo.png');
+
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('Email inválido').required('E-mail obrigatório'),
+  password: Yup.string().required('Senha obrigatória'),
+});
+
+const SignIn = (): JSX.Element => {
+  const dispatch = useDispatch();
+
+  const { isLoading, error } = useSelector((state: { user: UserState }) => state.user);
+
+  useEffect(() => {
+    if (!isLoading && error) {
+      showAlert({
+        title: 'Pokedex',
+        msg: error?.error || 'Houve um erro',
+      });
+
+      dispatch(errorResponse(null));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoading, error]);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async data => {
+    if (!isLoading) {
+      const { email, password } = data;
+
+      dispatch(signInRequest({ email, password }));
+    }
+  };
+
+  return (
+    <Container testID="viewsignin" source={background} resizeMode="stretch">
+      <KeyboardContent enabled={isIOS()} behavior="padding">
+        <Logo source={logo} resizeMode="contain" />
+        <Content>
+          <Title>Bem-vindo</Title>
+          <SubTitle>Insira os seus dados para acessar</SubTitle>
+
+          <Spacing vertical={10} />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label="Email"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                keyboardType="email-address"
+              />
+            )}
+            name="email"
+            defaultValue=""
+          />
+          {errors.email && <ErrorText testID="email-error">{errors.email.message}</ErrorText>}
+
+          <Spacing vertical={15} />
+
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <Input
+                label="Senha"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                isPassword
+              />
+            )}
+            name="password"
+            defaultValue=""
+          />
+          {errors.password && (
+            <ErrorText testID="password-error">{errors.password.message}</ErrorText>
+          )}
+
+          <Spacing vertical={15} />
+
+          <Button loading={isLoading} testID="button-submit" large onPress={handleSubmit(onSubmit)}>
+            Login
+          </Button>
+        </Content>
+      </KeyboardContent>
+    </Container>
+  );
+};
 
 export default SignIn;
--- src/scenes/SignIn/styles.ts
+++ src/scenes/SignIn/styles.ts
@@ -1,7 +1,42 @@
+import { Title as TitleRP } from 'react-native-paper';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+import { moderateScale } from '@utils/helpers';
+
+export const Container = styled.ImageBackground`
   flex: 1;
   justify-content: center;
-  align-items: center;
+  padding: 20px;
+`;
+
+export const KeyboardContent = styled.KeyboardAvoidingView`
+  flex: 1;
+  justify-content: space-around;
+`;
+
+export const Logo = styled.Image`
+  width: ${moderateScale(252)}px;
+  align-self: center;
+`;
+
+export const Content = styled.View`
+  background: ${props => props.theme.colors.background};
+  padding: ${props => props.theme.sizes.padding}px;
+  border-radius: ${props => props.theme.sizes.border}px;
+`;
+
+export const Title = styled(TitleRP)`
+  color: ${props => props.theme.colors.text};
+  font-weight: 600;
+  font-size: ${moderateScale(20)}px;
+`;
+
+export const SubTitle = styled(TitleRP)`
+  color: ${props => props.theme.colors.text};
+  font-weight: 300;
+  font-size: ${moderateScale(15)}px;
+`;
+
+export const ErrorText = styled.Text`
+  color: ${props => props.theme.colors.notification};
 `;
