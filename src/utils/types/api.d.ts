7dbbe0bcbdb4109593f2cd0de4e84cdc5c450ac8
Marcelo Barbosa <barbosa.marcelo6@gmail.com>
2021-10-15 11:48:21 -0300

    feat(services): create integration with api for pokemons and auth and type api

--- src/utils/types/api.d.ts
+++ src/utils/types/api.d.ts
@@ -1,33 +1,12 @@
 declare type User = {
-  id: string;
   name: string;
-  display_name?: string;
   email: string;
-  starterAffiliate?: string;
-  profilePhoto: string;
-  badge: 'rhino' | 'wolf' | 'eagle' | 'lion';
-  balance: number;
-  balanceAntecipation?: number;
-  balanceRelease?: number;
-  state: string | null;
-  country: string | null;
-  city: string | null;
-  zipcode: string | null;
-  telephone: string | null;
-  birthday: string;
-  date_create: string;
-  isChangePassword: boolean;
-  affiliatedOnStarter?: number;
-  occupation?: string | null;
-  bio?: string | null;
-  platforms: String[];
-  team_id?: string | null;
 };
 
 declare interface ApiResponse {
   message?: string;
   codeError?: string;
-  error?: boolean;
+  error?: string;
   data?: any;
   details?: any;
 }
@@ -37,5 +16,4 @@ declare interface UserState {
   token: string | null;
   error: ApiResponse | null;
   isLoading: boolean;
-  sendedRecoverPassword: boolean;
 }
